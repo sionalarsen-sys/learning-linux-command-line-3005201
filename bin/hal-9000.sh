3e0508bef50b43303e95d0393bd6cbe218df7950
sionalarsen-sys <siona.larsen@gmail.com>
2026-02-27 03:45:53 +0000

    adds hal script

---
+++ bin/hal-9000.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A simple script to demonstrate user input and variable use.
+echo "Hello. I'm Hal 9000. What is your name?"
+read USERNAME
+echo "I'm sorry $USERNAME, I'm afraid I can't open the pod bay doors."
